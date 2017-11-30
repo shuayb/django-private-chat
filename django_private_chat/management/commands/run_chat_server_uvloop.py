fb74e19d37c513edec7f2c68e79453d799c31198
Shuayb A <shuayb@gmx.com>
2017-12-01 02:00:03 +0500

    Fixed uvloop.

--- django_private_chat/management/commands/run_chat_server_uvloop.py
+++ django_private_chat/management/commands/run_chat_server_uvloop.py
@@ -1,35 +1,38 @@
 import asyncio
 import websockets
 import uvloop
+
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from django_private_chat import channels_uvloop as channels
-from django_private_chat import handlers
-from django_private_chat.utils import logger
+# from django_private_chat import channels_uvloop as channels
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
+from django_private_chat import channels
+from django_private_chat import handlers
+from django_private_chat.utils import logger
+
 
 class Command(BaseCommand):
-    help = 'Starts message center chat engine with uvloop'
+    help = 'Starts message center chat engine'
 
     def handle(self, *args, **options):
-        asyncio.async(
+        asyncio.ensure_future(
             websockets.serve(
                 handlers.main_handler,
                 settings.CHAT_WS_SERVER_HOST,
                 settings.CHAT_WS_SERVER_PORT
             )
         )
-
         logger.info('Chat server started')
 
-        asyncio.async(handlers.new_messages_handler(channels.new_messages))
+        asyncio.ensure_future(handlers.new_messages_handler(channels.new_messages))
         #asyncio.async(handlers.users_changed_handler(channels.users_changed))
-        asyncio.async(handlers.gone_online_handler(channels.gone_online))
-        asyncio.async(handlers.check_online_handler(channels.check_online))
-        asyncio.async(handlers.gone_offline_handler(channels.gone_offline))
-        #asyncio.async(handlers.is_typing_handler(channels.is_typing))
-        asyncio.async(handlers.read_message_handler(channels.read_unread))
+        asyncio.ensure_future(handlers.gone_online_handler(channels.gone_online))
+        asyncio.ensure_future(handlers.check_online_handler(channels.check_online))
+        asyncio.ensure_future(handlers.gone_offline_handler(channels.gone_offline))
+        asyncio.ensure_future(handlers.is_typing_handler(channels.is_typing))
+        asyncio.ensure_future(handlers.read_message_handler(channels.read_unread))
+        asyncio.ensure_future(handlers.unread_msg_count_handler(channels.unread_msg_count))
         loop = asyncio.get_event_loop()
         loop.run_forever()
